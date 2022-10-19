06839eec002294b270d5b7f46f1194c46697d203
yeasin-Ahammed-apon <yeasinahammedapon@gmail.com>
2022-10-19 19:06:34 +0600

    v-2

---
+++ p1/p1/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user, pass;
+            user = textBox1.Text;
+            pass = textBox2.Text;
+            if (user == "apon" && pass == "191071051")
+            {
+                Form1 f1 = new Form1();
+                f1.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Password");
+            }
+        }
+    }
+}
